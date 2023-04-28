9d155a7ab86aedf603d799c350e1d08fe183c5f7
rafiqislam319 <rafiqislam319@gmail.com>
2023-04-28 19:33:42 +0600

    implement login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -19,5 +20,21 @@ class AuthController extends Controller
             'password' => Hash::make($request->input('password')),
         ]);
         return response($user, Response::HTTP_CREATED);
+    }
+    public function login(Request $request){
+        if(!Auth::attempt($request->only('email', 'password'))){
+            return response([
+                'error' => 'Invalid Credentials'
+            ]);
+        }
+        $user = Auth::user();
+        $token = $user->createToken('token')->plainTextToken;
+        return response([
+            'jwt' => $token
+        ]);
+    }
+
+    public function user(Request $request){
+        return $request->user();
     }
 }
